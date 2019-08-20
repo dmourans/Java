9e965126485c7e74df3b71df6bb0316fe98a29b3
Deivid Moura <deividmourans@hotmail.com>
2019-08-20 18:56:10 -0300

    Exercicios de java com objetos

---
+++ Java/src/br/dmoura/dhouse/java/exercicios/javacomobjetos/jogadoresetreinamento/JogadorDeFutebol.java
@@ -0,0 +1,55 @@
+package br.dmoura.dhouse.java.exercicios.javacomobjetos.jogadoresetreinamento;
+
+public class JogadorDeFutebol {
+
+  private String nome;
+  private int energia;
+  private int alegria;
+  private int gols;
+  private int experiência;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
+    public int getAlegria() {
+        return alegria;
+    }
+
+    public void setAlegria(int alegria) {
+        this.alegria = alegria;
+    }
+
+    public int getGols() {
+        return gols;
+    }
+
+    public void setGols(int gols) {
+        this.gols = gols;
+    }
+
+    public int getExperiência() {
+        return experiência;
+    }
+
+    public void setExperiência(int experiência) {
+        this.experiência = experiência;
+    }
+
+    public void fazerGol (int qtdDeGols) {
+
+    }
+
+}
