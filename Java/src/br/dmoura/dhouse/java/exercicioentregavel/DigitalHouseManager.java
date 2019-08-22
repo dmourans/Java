f2b996b1e5678174254297e7465145c2ab35ca8c
Deivid Moura <deividmourans@hotmail.com>
2019-08-22 20:22:47 -0300

    Exercicio Entregável || Deivid Moura

---
+++ Java/src/br/dmoura/dhouse/java/exercicioentregavel/DigitalHouseManager.java
@@ -0,0 +1,12 @@
+package br.dmoura.dhouse.java.exercicioentregavel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DigitalHouseManager {
+    private List<Aluno> alunos = new ArrayList<>();
+    private List<Professor> professores = new ArrayList<>();
+    private List<Curso> cursos = new ArrayList<>();
+    private List<Matricula> matriculas = new ArrayList<>();
+
+}
